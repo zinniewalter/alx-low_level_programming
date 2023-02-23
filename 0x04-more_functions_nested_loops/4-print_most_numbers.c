3f3482f0c870c868703d475fcdc562af462006c4
zinniewalter <waltermarian24@gmail.com>
2023-02-23 10:19:12 -0800

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,12 +12,13 @@ void print_most_numbers(void)
 	for (c = 48; c < 58; c++)
 	{
 		if (c != 50)
-	{
-		if (c != 50)
-	{
-		_putchar(c);
-	}
-	}
+		{
+			if (c != 50)
+			{
+				_putchar(c);
+			}
+		}
 	}
 	_putchar('\n');
 }
+
