ec48f2bb6836e9972ff673c356f7171d0bdb5423
zinniewalter <waltermarian24@gmail.com>
2023-02-27 13:49:30 -0800

    my header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
