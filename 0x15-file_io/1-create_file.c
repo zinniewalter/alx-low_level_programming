c74222abee0a2831ac382ab6204fe9d0d62bf3ae
zinniewalter <waltermarian24@gmail.com>
2023-04-07 14:24:30 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ * create_file - creates a file with the specified text content
+ * @filename: the name of the file to create
+ * @text_content: the text to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+	mode_t old_mask;
+
+	if (filename == NULL)
+		return (-1);
+
+	old_mask = umask(0);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
+	umask(old_mask);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
