1f8f8d934b2dc90eeee50795fc4812f50a552b31
zinniewalter <waltermarian24@gmail.com>
2023-05-08 14:01:11 -0700

    fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,8 +53,10 @@ int main(int argc, char *argv[])
 	{
 		nchars = read(file_from, buf, 1024);
 			if (nchars == -1)
+			{
 				check_file_access(-1, 0, argv);
 				nwr = write(file_to, buf, nchars);
+			}
 
 			if (nwr == -1)
 				check_file_access(0, -1, argv);
